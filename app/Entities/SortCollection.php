ff8a1506c82c4a634f896cb4bd012f2bd15c97f1
DanielReis <idanielreiss@gmail.com>
2019-09-26 22:47:09 -0300

    Solved

--- app/Entities/SortCollection.php
+++ app/Entities/SortCollection.php
@@ -14,6 +14,8 @@ class SortCollection
     public $emails;
     public $folders;
 
+    public $rootFolders;
+
     public function __construct(string $email)
     {
         $this->emails = new Email($email);
@@ -21,11 +23,65 @@ class SortCollection
     }
 
     public function getResult(){
+        $this->emails->load();
+        $this->folders->load();
 
+        $sortedFolders = $this->getSortedFolders();
+        $sortedEmails = $this->getFolderedMailsBySize();
 
-        return true;
+        return $this->getFoldersBySize($sortedFolders,$sortedEmails);
     }
 
+    public function getSortedFolders(){
+        $folderAux = [];
+        foreach($this->folders as $folder){
+            if(!array_key_exists($folder->parentId,$folderAux)){
+                $folderAux[$folder->parentId][] = $folder->id;
+                continue;
+            }
+            $folderAux[$folder->parentId][] = $folder->id;
+        }
+        $this->rootFolders = $folderAux[0];
+        unset($folderAux[0]);
+        return $folderAux;
+    }
 
+    public function getFolderedMailsBySize(){
+        $emailAux = [];
+
+        foreach($this->emails as $email){
+            if(!array_key_exists($email->folderId,$emailAux)){
+                $emailAux[$email->folderId]['size'] = $email->size;
+                continue;
+            }
+            $emailAux[$email->folderId]['size'] += $email->size;
+        }
+
+        return $emailAux;
+    }
+
+    public function getFoldersBySize($folders,$sizedMails){
+        $result = [];
+        foreach($this->rootFolders as $value){
+            if(!in_array($value,$result)){
+                $result[$value] = $sizedMails[$value]['size'];
+                continue;
+            }
+            $result[$value] += $sizedMails[$value]['size'];
+        }
+        foreach($folders as $folderKey => $folder){
+            foreach ($folder as $value){
+                $result[$folderKey] += $sizedMails[$value]['size'];
+            }
+        }
+
+        arsort($result);
+        $resultSocorro = [];
+        foreach($result as $key => $value){
+            $resultSocorro[] = $key;
+        }
+
+        return $resultSocorro;
+    }
 
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,19 +9,27 @@ class ExampleTest extends TestCase
 
     public function getTestData(): array{
         return [
+            [
                 'email' => 'daniel@he4rt.com',
                 'solvedArray' => [3,1,2]
+            ],
+            [
+                'email' => 'gustavo@he4rt.com',
+                'solvedArray' => [1,3,2]
+            ]
         ];
     }
 
     public function testExample()
     {
         $expected = $this->getTestData();
-        $sended = new \App\Entities\SortCollection($expected['email']);
+        foreach($expected as $key => $test){
+            $sended = new \App\Entities\SortCollection($test['email']);
+            $this->assertEquals(
+                $expected[$key]['solvedArray'], $sended->getResult()
+            );
+        }
 
 
-        $this->assertEquals(
-            $expected['solvedArray'], $sended->getResult()
-        );
     }
 }
